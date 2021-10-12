549a20156f6660e6c08758f6244a8fd0b3696f08
Genival Silva <genivalvb@yahoo.com.br>
2021-10-12 20:47:06 -0300

    Put port 8080 like default

--- server.js
+++ server.js
@@ -8,3 +8,10 @@ function requireHTTPS(req, res, next) {
 const express = require('express');
 const app = express();
 app.use(requireHTTPS);
+app.use(express.static('./dist/pandemic-interface-app'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/pandemic-interface-app/'}),
+);
+
+app.listen(process.env.PORT || 8080);
